bf1bb8730a7b3164e8914cb27d6a27b3b1b4e856
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-18 23:51:51 -0500

    Creating the routes for CRUD operations on the authors

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,11 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
+$router->get('/authors/{author}', 'AuthorController@show');
+$router->put('/authors/{author}', 'AuthorController@update');
+$router->patch('/authors/{author}', 'AuthorController@update');
+$router->delete('/authors/{author}', 'AuthorController@destroy');
