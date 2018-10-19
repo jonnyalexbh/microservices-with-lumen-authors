0a536100aa356ed67a30bfb51db350f65759fc87
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-19 09:04:36 -0500

    Normalizing the authors’ microservice responses

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
+
     /**
      * __construct
      *
