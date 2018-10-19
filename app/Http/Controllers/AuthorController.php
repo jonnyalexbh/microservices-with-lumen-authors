a4a703052e6b1c4478f769a793cba49c000b31d0
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-19 09:49:40 -0500

    Showing the list of authors from the Lumen controller

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
@@ -24,6 +25,9 @@ class AuthorController extends Controller
      */
     public function index()
     {
+        $authors = Author::all();
+
+        return $this->successResponse($authors);
     }
 
     /**
